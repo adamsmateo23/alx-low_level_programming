f5da6918030a40d5ccf0c2a701df47eeb38650d4
Adams <akilifyproject@gmail.com>
2023-03-16 12:09:22 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * Main function has no return value
+ * Main is the entry point of this program
+ *
+ * Return is always set to 0(success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
