53ccbf97353185271c8c0124fa0a4877aa260411
Adams <akilifyproject@gmail.com>
2023-03-16 12:37:29 -0700

    with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-/*
- * Main function has no return value
- * Main is the entry point of this program
+#include<stdio.h>
+/**
+ * main - Entry point
  *
- * Return is always set to 0(success)
+ * Return: Always 0 (Success)
  */
-int main(void)
+int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
