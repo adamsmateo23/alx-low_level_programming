6b0a2703fa447f7413f7259e80c32337900efff0
Adams <akilifyproject@gmail.com>
2023-03-21 10:48:19 -0700

    First three questions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main ()
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
 {
-
-
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
